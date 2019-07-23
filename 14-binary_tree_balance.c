4b6ec7fbc649b27466734f0e82753b13818d8f23
AndZapata <andzapata514@gmail.com>
2019-07-23 08:03:03 -0500

    add all

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Comentario
+ *
+ * @parent: Comentario
+ * @value: Comentario
+ * Return: Comentario
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h_left;
+	size_t h_right;
+
+	h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (h_left > h_right ? h_left : h_right);
+}
+
+/**
+* binary_tree_insert_right - Comentario
+ *
+ * @parent: Comentario
+ * @value: Comentario
+ * Return: Comentario
+ */
+int binary_tree_balance(const binary_tree_t\
+ *tree)
+{
+	int bal_left = 1, bal_right = 1;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		bal_left += binary_tree_height(tree->left);
+	if (tree->right)
+		bal_right += binary_tree_height(tree->right);
+	return (bal_left - bal_right);
+}
