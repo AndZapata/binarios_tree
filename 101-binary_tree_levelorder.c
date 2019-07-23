5c2cbae8a2e62c39c2fdcebe843b400de9ec34f7
Jeniffervp <737@holbertonschool.com>
2019-07-23 23:29:15 +0000

    [Fix]spaces and remove else

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -14,16 +14,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
+
+	if (h_left > h_right)
+		return (h_left + 1);
 	else
-	{
-
-		 h_left = binary_tree_height(tree->left);
-		 h_right = binary_tree_height(tree->right);
-
-		if (h_left > h_right)
-			return(h_left + 1);
-		else return(h_right + 1);
-	}
+		return (h_right + 1);
 }
 
 /**
