e25fc52749ceebf4f0d24b7865d9a4b2bd3e5b2a
Jeniffervp <737@holbertonschool.com>
2019-07-23 19:02:59 +0000

    [Fix]delete else and add betty comments

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Comentario
+ * binary_tree_depth - function that measures the depth of a node
+ * in a binary tree
  *
- * @parent: Comentario
- * @value: Comentario
- * Return: Comentario
+ * @tree:  is a pointer to the node to measure the depth
+ * Return: if tree is NULL, your function must return 0.
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
@@ -14,10 +14,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	else
-	{
-		for (temp = tree; temp && temp->parent; temp = temp->parent)
-			i++;
-		return (i);
-	}
+
+	for (temp = tree; temp && temp->parent; temp = temp->parent)
+		i++;
+	return (i);
 }
--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,36 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Comentario
+ * binary_tree_nodes - function that counts the nodes with at least
+ * 1 child in a binary tree
  *
- * @parent: Comentario
- * @value: Comentario
- * Return: Comentario
+ * @tree: pointer to the root node of the tree to count the number of nodes
+ * Return: if tree is NULL and if pointer is not a node, return null.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t count = 1;
+
 	if (tree == NULL)
 		return (0);
-	else
+
+	if (tree->left && tree->right)
 	{
-		if (tree->left && tree->right)
-		{
-			count += binary_tree_nodes(tree->left) +
-				binary_tree_nodes(tree->right);
-			return (count);
-		}
-		else if (tree->left)
-		{
-			count += binary_tree_nodes(tree->left);
-			return (count);
-		}
-		else if (tree->right)
-		{
-			count += binary_tree_nodes(tree->right);
-			return (count);
-		}
-		else
-			return (0);
+		count += binary_tree_nodes(tree->left) +
+			binary_tree_nodes(tree->right);
+		return (count);
 	}
+	else if (tree->left)
+	{
+		count += binary_tree_nodes(tree->left);
+		return (count);
+	}
+	else if (tree->right)
+	{
+		count += binary_tree_nodes(tree->right);
+		return (count);
+	}
+	else
+		return (0);
 }
